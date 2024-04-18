4e8b42f0d5b5b232e17cc72287503dd680ffa555
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-04-17 20:45:24 -0700

    Project submission

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-var app = express();
+const app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
+app.route('/api/fileanalyse')
+  .post(upload.single('upfile'), (req, res) => {
+    if (req.file) {
+      res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+      })
+    } else {
+      res.json({ error: 'No file selected' })
+    }
+  })
 
+const port = process.env.PORT || 3000
 
-
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
